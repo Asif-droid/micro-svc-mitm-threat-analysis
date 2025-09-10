0efab97f6938759ba03a4fbe31d641b798a7d6b6
K M Asifur Rahman <asifurbuet98@gmail.com>
2025-09-10 06:24:05 +0600

    added report

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,6 +1,13 @@
-kubectl apply -f flask-app-deployment.yml
-kubectl apply -f flask-app2-deployment.yml
-kubectl apply -f istio-gateway.yml
+kubectl apply -f flask-app-deployment-def.yml
+kubectl apply -f flask-app2-deployment-def.yml
+kubectl apply -f istio-gateway-def.yml
+
+## attack
+# kubectl apply -f mitm-attacker.yml
+# kubectl delete service flask-app-2
+# kubectl delete service mitm-flask-app-2
+# kubectl expose deployment mitm-proxy --name=flask-app-2 --port=5001--target-port=5001
+# kubectl expose deployment flask-app-2 --name=flask-app-2-backup --port=5001 --target-port=5001
 
 
 
